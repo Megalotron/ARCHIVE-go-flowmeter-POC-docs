a5577e53149539ec1ce048653da768bfee21bfd6
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-10-03 00:06:27 -0700

    feat: add minimal documentation
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Easy to Use',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        go-flowmeter is written in go, meaning that you can easily have a look at the code and understand its behaviour
+          by yourself. It also features a very simple CLI to focus on efficiency and simplicity.
+      </>
+    ),
+  },
+  {
+    title: 'Focus on What Matters',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        go-flowmeter has one and only one goal: ethernet traffic flow generation and analyse for anomaly detection. Nothing
+          more, nothing less.
+      </>
+    ),
+  },
+  {
+    title: 'Powered by Megalotron',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Created by the team at Megalotron, we are focused on trying to solve one of the most challenging topic in AI +
+          security today : anomaly detection on internet traffics.
+      </>
+    ),
+  },
+];
+
+function Feature({title, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
