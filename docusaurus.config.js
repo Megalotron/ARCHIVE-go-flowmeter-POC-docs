253e924aeb6bca035a8ee1941d5bb41902076c37
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-10-03 00:14:10 -0700

    fix: update wrong baseURL
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'go-flowmeter-docs',
   tagline: 'A documentation for go-flowmeter',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://megalotron.github.io/go-flowmeter-docs/',
+  baseUrl: '/go-flowmeter-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Megalotron', // Usually your GitHub org/user name.
+  projectName: 'go-flowmeter-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
